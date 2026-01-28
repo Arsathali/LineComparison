afb31ebc3bcdd90d3183f621d8395b903f3368e7
arsath ali <arsatharsha17@gmail.com>
2026-01-28 15:24:55 +0530

    Add equals method to compare line equality

--- src/com/linecomparison/Line.java
+++ src/com/linecomparison/Line.java
@@ -1,5 +1,6 @@
 
 public class Line {
+
     private final Point startPoint;
     private final Point endPoint;
 
@@ -9,9 +10,26 @@ public class Line {
     }
 
     public double calculateLength() {
-    int xDifference = endPoint.x - startPoint.x;
-    int yDifference = endPoint.y - startPoint.y;
-    
-    return Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
-}
+        int xDifference = endPoint.x - startPoint.x;
+        int yDifference = endPoint.y - startPoint.y;
+        
+        return Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Line otherLine = (Line) obj;
+
+        return this.calculateLength() == otherLine.calculateLength();
+    }
+
 }
