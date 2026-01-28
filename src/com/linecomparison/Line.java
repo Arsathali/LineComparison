3b85ab251448520734ae4c8775c24455a67b0923
arsath ali <arsatharsha17@gmail.com>
2026-01-28 15:20:11 +0530

    Add line length calculation

--- src/com/linecomparison/Line.java
+++ src/com/linecomparison/Line.java
@@ -7,4 +7,11 @@ public class Line {
         this.startPoint = startPoint;
         this.endPoint = endPoint;
     }
+
+    public double calculateLength() {
+    int xDifference = endPoint.x - startPoint.x;
+    int yDifference = endPoint.y - startPoint.y;
+    
+    return Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
+}
 }
