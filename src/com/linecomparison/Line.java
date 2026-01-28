8f24da25b63db5c899bd4350a41b4a4d4254092f
arsath ali <arsatharsha17@gmail.com>
2026-01-28 15:11:01 +0530

    Add Line and Point classes for line creation

---
+++ src/com/linecomparison/Line.java
@@ -0,0 +1,10 @@
+
+public class Line {
+    private final Point startPoint;
+    private final Point endPoint;
+
+    public Line(Point startPoint, Point endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+}
